70cc7a0ddec05cf376d5a1e04f2fe06a39cbbc8f
CLJ5059A <jeremy.claux@univ-tlse3.fr>
2024-09-18 15:39:08 +0200

    TP1

---
+++ src/personnages/Druide.java
@@ -0,0 +1,36 @@
+package personnages;
+
+import java.util.Random;
+
+public class Druide {
+	private String nom;
+	private int forcePotion;
+	private int effetPotionMin;
+	private int effetPotionMax;
+	
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		super();
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " 
+				+ effetPotionMin + " à " + effetPotionMax +".");
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<< " + texte + " >>");
+	}
+	
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+	
+	private int preparerPotion() {
+		Random random = new Random();
+		
+	}
+}
